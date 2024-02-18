a7843bfde8209ffe754a591af7e4fc685c957bb3
Shubham Dubey <subham02101997@gmail.com>
2024-02-18 17:56:39 +0530

    hold portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import SplashScreen from './components/splashScreen';
+import Experiments from './components/experiments/experiments';
+import Resume from './components/resume';
 
 function App() {
   return (
     <div className="App">
-      Namaste
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<SplashScreen />} />
+        <Route element={<Home />}>
+          <Route path='/home' element={<Resume />} />
+          <Route path='/experiments' element={<Experiments />} />
+        </Route>
+
+      </Routes>
     </div>
   );
 }
